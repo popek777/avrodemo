db759e99dc0207f9b39137fb143959e97c41426e
Marcin Wrobel <marwrobel@gmail.com>
2022-12-07 09:51:24 +0100

    decode witch generic datum and schema

--- avrodemo.cpp
+++ avrodemo.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 extern void test_encode_decode_generic_datatype_with_traits();
+extern void test_decode_generic_datatype_with_schema();
 
 int main(int argc, char **argv) {
 
   test_encode_decode_generic_datatype_with_traits();
+  test_decode_generic_datatype_with_schema();
 
   return 0;
 }
--- generic_type_usecase.cpp
+++ generic_type_usecase.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <sstream>
 
 #include "avro/Decoder.hh"
 #include "avro/Encoder.hh"
 #include "avro/Specific.hh"
+#include "avro/Compiler.hh"
+#include "avro/Generic.hh"
 
+namespace {
 struct SomeType {
   int idx{0};
   std::string name;
@@ -14,6 +18,7 @@ std::ostream &operator<<(std::ostream &os, const SomeType &obj) {
   os << '(' << obj.idx << ", " << obj.name << ", " << obj.value << ')';
   return os;
 }
+} // namespace
 
 // trait to implemente encode/decode 'strategy' for custom type
 namespace avro {
@@ -43,7 +48,7 @@ void test_encode_decode_generic_datatype_with_traits() {
          "encoding decoding\n"
          "Would be nicer to have singe func with & operator which would be "
          "then "
-         "implemented in encoder/decoder (like in cereal library)"
+         "implemented in encoder/decoder (like in cereal library)\n"
       << std::endl;
 
   SomeType src{34, "lorem ipsum", 45.234};
@@ -65,3 +70,73 @@ void test_encode_decode_generic_datatype_with_traits() {
   std::cout << "dest: " << src << std::endl;
 }
 
+void test_decode_generic_datatype_with_schema()
+{
+  std::cout
+      << "====================================================================\\n"
+         "=== encode generic datatype manually and decode with matching schema\n"
+         "====================================================================\\n"
+      << std::endl;
+
+    SomeType src{34, "lorem ipsum", 45.234};
+    std::cout << "src: " << src << std::endl;
+
+    std::unique_ptr<avro::OutputStream> out = avro::memoryOutputStream();
+    avro::EncoderPtr e = avro::binaryEncoder();
+    e->init(*out);
+    avro::encode(*e, src);
+
+    std::unique_ptr<avro::InputStream> in = avro::memoryInputStream(*out);
+    avro::DecoderPtr d = avro::binaryDecoder();
+    d->init(*in);
+
+    SomeType dest;
+
+    constexpr const char* schemaText = 
+R"({
+    "type": "record",
+    "name": "some_name_for_some_type",
+    "fields" : [
+        {"name": "some_id", "type": "int"},
+        {"name": "some_name", "type" : "string"}
+        {"name": "some_value", "type" : "double"}
+    ]
+})";
+    std::istringstream iss(schemaText);
+    avro::ValidSchema schema;
+    avro::compileJsonSchema(iss, schema);
+    avro::GenericDatum datum(schema);
+    
+    avro::decode(*d, datum);
+
+    std::cout << "Type: " << datum.type() << std::endl;
+    if (datum.type() != avro::AVRO_RECORD) {
+      throw std::runtime_error{"should be record!"};
+    }
+
+    const avro::GenericRecord &record = datum.value<avro::GenericRecord>();
+    std::cout << "Field-count: " << record.fieldCount() << std::endl;
+
+    {
+      const avro::GenericDatum &field0 = record.fieldAt(0);
+      std::cout << "field0 type: " << field0.type()
+                << ", value: " << field0.value<int>() << std::endl;
+      dest.idx = field0.value<int>();
+    }
+
+    {
+      const avro::GenericDatum &field1 = record.fieldAt(1);
+      std::cout << "field1 type: " << field1.type()
+                << ", value: " << field1.value<std::string>() << std::endl;
+      dest.name = field1.value<std::string>();
+    }
+
+    {
+      const avro::GenericDatum &field2 = record.fieldAt(2);
+      std::cout << "field2 type: " << field2.type()
+                << ", value: " << field2.value<double>() << std::endl;
+      dest.value = field2.value<double>();
+    }
+
+    std::cout << "dest: " << dest << std::endl;
+}
